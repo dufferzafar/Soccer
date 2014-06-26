b39e1ecdd4901ca88dcdf5b3087bb5dc27eacaf5
Shadab Zafar <dufferzafar0@gmail.com>
2014-06-27 02:21:10 +0530

    Created a script to download project list as JSON
    Turns out there's a simple way to download the data (that's used by the
    melange site itself) - you just have to keep downloading data from
    http://www.google-melange.com/gsoc/org/list/public/google/gsoc2014?fmt=json&limit=100&start={GIANT STRING HERE}
    
    Until the "next" property in the json is "done".

---
+++ Data/Projects/Projects.py
@@ -0,0 +1,57 @@
+#!python3
+
+import os
+import json
+import urllib.request as UR
+import urllib.parse as UP
+
+# The melange website
+root = "https://www.google-melange.com"
+
+# Let the game begin
+for year in range(2009, 2015):
+
+    print("Year: " + str(year))
+
+    # Loop Controls
+    nxt, start, end = "", 1, 100
+
+    # Create the folder if it doesn't already exist
+    if not os.path.isdir(str(year)):
+        os.mkdir(str(year))
+
+    # The Ideas URL
+    ideas = "/gsoc/projects/list/google/gsoc" + str(year) + "?"
+
+    # Gotta catch 'em all
+    while nxt != "done":
+
+        # Path to the current json file
+        json_name = os.path.join(str(year), str(start)+"-"+str(end)+".json")
+
+        # Thank you melange, for creating a nice json interface
+        params = UP.urlencode({'fmt':'json', 'limit':'100', 'idx':'0', '_':'1403807190239', "start": nxt})
+
+        # Download only missing pieces of the puzzle
+        if os.path.isfile(json_name):
+            print("Existing file: " + json_name)
+            with open(json_name, "r") as json_file:
+                json_data = json.load(json_file)
+        else:
+            url = root + ideas + params
+            print("Downloading file: " + json_name + ", from: " + url)
+            with open(json_name, "w") as json_file:
+                site = UR.urlopen(url)
+                json_data = json.loads(site.read().decode())
+                json.dump(json_data, json_file, sort_keys=True, indent=2)
+
+        # Next, please
+        nxt = json_data["next"]
+        start, end = start+100, end+100
+
+        print("next: " + nxt + "\n")
+
+    print("--------------------------------------------------------------")
+
+# Whew!!
+print("All Done!")
