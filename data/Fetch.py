b174afccc1ef9908999fed554625ce3ad18a6193
Shadab Zafar <dufferzafar0@gmail.com>
2014-06-29 03:08:48 +0530

    Fixed a bug in Fetch
    ...that happened when 'projects' or 'org' directories didn't exist.

--- data/Fetch.py
+++ data/Fetch.py
@@ -10,7 +10,10 @@ import urllib.request as UR
 import urllib.parse as UP
 
 # What to fetch? 'projects' or 'org'?
-fetch = 'org'
+what = 'org'
+
+if not os.path.isdir(what):
+    os.mkdir(what)
 
 # The melange website
 root = "https://www.google-melange.com"
@@ -24,8 +27,8 @@ for year in range(2009, 2015):
     nxt, start, end = "", 1, 100
 
     # Create the folder if it doesn't already exist
-    if not os.path.isdir(os.path.join(fetch, str(year))):
-        os.mkdir(os.path.join(fetch, str(year)))
+    if not os.path.isdir(os.path.join(what, str(year))):
+        os.mkdir(os.path.join(what, str(year)))
 
     # The Ideas URL
     ideas = "/gsoc/projects/list/google/gsoc" + str(year) + "?"
@@ -34,7 +37,7 @@ for year in range(2009, 2015):
     while nxt != "done":
 
         # Path to the current json file
-        json_name = os.path.join(os.path.join(fetch, str(year)), str(start)+"-"+str(end)+".json")
+        json_name = os.path.join(os.path.join(what, str(year)), str(start)+"-"+str(end)+".json")
 
         # Thank you melange, for creating a nice json interface
         params = UP.urlencode({'fmt':'json', 'limit':'100', 'idx':'0', '_':'1403807190239', "start": nxt})
